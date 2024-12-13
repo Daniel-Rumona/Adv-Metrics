566e85990c9469e75f6b0e311581aeb465192798
Daniel-Rumona <danielrumona@gmail.com>
2024-12-13 11:12:36 +0200

    Update mockData.tsx

--- src/data/mockData.tsx
+++ src/data/mockData.tsx
@@ -1,10 +1,10 @@
 export const mockData = {
   dashboard: {
     summaryMetrics: [
-      { metric: 'Advertising Value Equivalent', value: 15 },
+      { metric: 'Advertising Value Equivalent', value: 6000 },
       { metric: 'Active Users', value: 8760 },
       { metric: 'Engagement Rate', value: 0.35 },
-      { metric: 'Reputation Index', value: 0.45 }
+      { metric: 'Reputation Index', value: 150031.2 }
     ]
   },
   mockCampaignActivity: [
